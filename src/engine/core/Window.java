bd04188d6e5f6c6a7e01059848524403cf862113
TheRockstarr33 <drockstarr33@yahoo.com>
2018-03-17 12:39:37 +0400

    Added basic quad rendering

--- src/engine/core/Window.java
+++ src/engine/core/Window.java
@@ -1,6 +1,7 @@
 package engine.core;
 
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.GL;
 
 public class Window {
@@ -11,13 +12,28 @@ public class Window {
             System.exit(1);
         }
 
-        long win = glfwCreateWindow(640, 480, "Djent", 0, 0); //Width, Height, Title, Fullscreen, ?
+        long win = glfwCreateWindow(640, 480, "Djent", 0, 0); //Width, Height, Title, Fullscreen, Monitor for window
         glfwShowWindow(win);
+
+        glfwMakeContextCurrent(win);
 
         GL.createCapabilities();
 
         while (glfwWindowShouldClose(win) != true) {
             glfwPollEvents();
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            //This code is deprecated, must be changed later. it is being used now just for testing purposes.
+            glBegin(GL_QUADS);
+
+            glVertex2f(-0.5f, 0.5f);
+            glVertex2f(0.5f, 0.5f);
+            glVertex2f(0.5f, -0.5f);
+            glVertex2f(-0.5f, -0.5f);
+            glEnd();
+
+            glfwSwapBuffers(win);
         }
 
         glfwTerminate();
